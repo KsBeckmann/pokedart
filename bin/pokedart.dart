37590817b8dc249f209df27b35086d27c979549e
emannoel-2023 <emannoel.2023@gmail.com>
2026-04-03 18:41:03 -0300

    navecacao menu por questoes

--- bin/pokedart.dart
+++ bin/pokedart.dart
@@ -10,6 +10,50 @@ part 'teste_questoes_01_a_11.dart';
 part 'questao_12.dart';
 
 void main() {
-  teste_ate_a_questao_11();
-  questao_12();
+  exibir_menu();
+}
+
+void exibir_menu() {
+  while (true) {
+    print('================ MENU POKEDART ================');
+    print('1 - Rodar testes das questoes 1 a 11');
+    print('2 - Rodar questao 12');
+    print('3 - Rodar tudo');
+    print('0 - Sair');
+    print('===============================================');
+    stdout.write('Escolha uma opcao: ');
+
+    var opcao = stdin.readLineSync()?.trim();
+    print('');
+
+    switch (opcao) {
+      case '1':
+        teste_ate_a_questao_11();
+        _aguardar_enter();
+        break;
+      case '2':
+        questao_12();
+        _aguardar_enter();
+        break;
+      case '3':
+        teste_ate_a_questao_11();
+        print('');
+        questao_12();
+        _aguardar_enter();
+        break;
+      case '0':
+        print('Encerrando o programa...');
+        return;
+      default:
+        print('Opcao invalida. Tente novamente.');
+        print('');
+    }
+  }
+}
+
+void _aguardar_enter() {
+  print('');
+  stdout.write('Pressione Enter para voltar ao menu... ');
+  stdin.readLineSync();
+  print('');
 }
