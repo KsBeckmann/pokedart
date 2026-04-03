9fcdc28831c35f0d6b6425f39de4404994ffd7b5
emannoel-2023 <emannoel.2023@gmail.com>
2026-04-03 18:51:36 -0300

    att menu de navegacao

--- bin/pokedart.dart
+++ bin/pokedart.dart
@@ -16,9 +16,19 @@ void main() {
 void exibir_menu() {
   while (true) {
     print('================ MENU POKEDART ================');
-    print('1 - Rodar testes das questoes 1 a 11');
-    print('2 - Rodar questao 12');
-    print('3 - Rodar tudo');
+    print('1 - Rodar questao 1');
+    print('2 - Rodar questao 3');
+    print('3 - Rodar questao 4');
+    print('4 - Rodar questao 5');
+    print('5 - Rodar questao 6');
+    print('6 - Rodar questao 7');
+    print('7 - Rodar questao 8');
+    print('8 - Rodar questao 9');
+    print('9 - Rodar questao 10');
+    print('10 - Rodar questao 11');
+    print('11 - Rodar questao 12');
+    print('12 - Rodar questoes 1 a 11');
+    print('13 - Rodar tudo');
     print('0 - Sair');
     print('===============================================');
     stdout.write('Escolha uma opcao: ');
@@ -28,14 +38,54 @@ void exibir_menu() {
 
     switch (opcao) {
       case '1':
-        teste_ate_a_questao_11();
+        rodar_questao_1();
         _aguardar_enter();
         break;
       case '2':
-        questao_12();
+        rodar_questao_3();
         _aguardar_enter();
         break;
       case '3':
+        rodar_questao_4();
+        _aguardar_enter();
+        break;
+      case '4':
+        rodar_questao_5();
+        _aguardar_enter();
+        break;
+      case '5':
+        rodar_questao_6();
+        _aguardar_enter();
+        break;
+      case '6':
+        rodar_questao_7();
+        _aguardar_enter();
+        break;
+      case '7':
+        rodar_questao_8();
+        _aguardar_enter();
+        break;
+      case '8':
+        rodar_questao_9();
+        _aguardar_enter();
+        break;
+      case '9':
+        rodar_questao_10();
+        _aguardar_enter();
+        break;
+      case '10':
+        rodar_questao_11();
+        _aguardar_enter();
+        break;
+      case '11':
+        questao_12();
+        _aguardar_enter();
+        break;
+      case '12':
+        teste_ate_a_questao_11();
+        _aguardar_enter();
+        break;
+      case '13':
         teste_ate_a_questao_11();
         print('');
         questao_12();
